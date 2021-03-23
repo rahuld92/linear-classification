4bf37a3121eb7c212a2e844f0e7635d9a7e254f9
Rahul Dutt <>
2021-03-23 18:45:26 +1200

    Fix Pseudo Inverse Logic

--- main.py
+++ main.py
@@ -2,28 +2,26 @@ from sklearn.datasets import make_regression
 import matplotlib.pylab as plt
 import numpy as np
 
-x, y = make_regression(n_samples=100, n_features=1, noise=22)
+x_data, y_data = make_regression(n_samples=100, n_features=1, noise=22)
 
-x_mean = np.mean(x)
-y_mean = np.mean(y)
 
-num = 0
-den = 0
-for i in range(len(x)):
-    num += (x[i] - x_mean) * (y[i] - y_mean)
-    den += (x[i] - x_mean) ** 2
-coefficient = num / den
-intercept = y_mean - (coefficient * x_mean)
+plt.scatter(x_data, y_data)
+x = np.c_[np.ones(x_data.shape[0]), x_data]
+X = np.linalg.pinv(x)
 
-y_pred = intercept + coefficient * x
+w = X @ y_data
+# w = np.insert(w, 0, 1)
+w = w.tolist()
 
-plt.scatter(x, y)
-plt.plot(x, y_pred, 'r')
+slope = -(w[1]) / (w[0])
+intercept = -w[0] / w[1]
+
+y_pred = (slope * x) + intercept
+plt.plot(x, y_pred)
+
 plt.show()
-
 ssr = 0
+for i in range(len(y_data)):
+    ssr += (y_data[i] - y_pred[i])**2
 
-for i in range(len(x)):
-    ssr += (y[i] - y_pred[i]) ** 2
-
-print(f'Sum of Square Error: {ssr}')
+print(f'Sum of Error Squared {ssr}')
